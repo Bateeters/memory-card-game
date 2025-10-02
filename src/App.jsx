b117da22923efa794fd8390033200d5d1258e861
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-10-02 18:52:40 -0400

    12 random pokemon are being generated with images and names.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,46 @@ import './App.css'
 function App() {
   const [cards, setCards] = useState([]); // empty array to save card images
 
+  useEffect(() => {
+    async function fetchPokemon() { // creating async function
+      const promises = []; // storage for each api return
+
+      // loop to generate 12 calls
+      for (let i = 1; i <= 12; i++){
+        const randNum = Math.floor(Math.random() * (151 - 1 + 1)) + 1; // generates a random number for each loop
+        promises.push(
+          fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`).then((res) => // uses random number to populate pokemon info
+            res.json()
+          )
+        );  
+      }
+
+      // storing all the api info in "results"
+      const results = await Promise.all(promises);
+
+      // iterate through results to store id, name, and image for each pokemon
+      const newCards = results.map((pokemon) => ({
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.front_default,
+      }));
+
+      setCards(newCards); // adding each card to the "cards" useState
+    }
+
+    fetchPokemon(); // calling function to populate 12 random pokemon
+  }, []);
+
   return (
     <div>
       <h1>Memory Card Game</h1>
       <div className='card-grid'>
-        <p>Cards will populate here</p>
+        {cards.map((card) => (
+          <div key={card.id} className='card' style={{border: "1px solid white"}}>
+            <img src={card.image} alt={card.name} />
+            <h3>{card.name}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
